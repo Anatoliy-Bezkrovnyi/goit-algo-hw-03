f637d69fa2be82b155657a76158ac30de2399f01
Anatoliy-Bezkrovnyi <bezkrovnyi1@gmail.com>
2024-01-23 21:21:49 +0200

    FIxed inconcistancies at HW 1

--- home_task_01.py
+++ home_task_01.py
@@ -1,4 +1,6 @@
 from queue import Queue
+import random
+import time
 
 #Створити чергу заявок
 requests_queue = Queue()
@@ -17,16 +19,24 @@ def process_request():
     while not requests_queue.empty():
         handled_request = requests_queue.get()
         print(f"{handled_request} was taken for processing.")
+        time.sleep(1)
     else:
-        print("All requests we handled successfully.")
+        print("All requests we handled, there are no items in queue to process.")    
 
 
 while True:
-    command = input("Enter the command: ")
-    if command == "generate": 
-        for _ in range(5):
+    try:
+        requests_to_generate = random.randint(1, 5)
+        for _ in range(requests_to_generate):
             generate_request()
-    elif command == "solve": 
-        process_request()
-    elif command == "stop": 
+            time.sleep(1)
+        
+        requests_to_process = random.randint(1, 5)
+        for _ in range(requests_to_process):
+            process_request()
+            time.sleep(1)
+            
+        
+    except KeyboardInterrupt:
+        print("Execution was interrupted by pressing ctrl-c combination.")
         break
